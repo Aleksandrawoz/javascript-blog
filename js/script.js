47d43d853cf7de4d2c730500eb14fad729a27b89
ola wozniak <olawozniak551@gmail.com>
2021-06-30 18:28:46 +0200

    remove class active, all article links

--- js/script.js
+++ js/script.js
@@ -3,27 +3,39 @@
     console.log('links:', links);
   });*/
 const titleClickHandler = function (event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
+
     /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
+
+
     /* [IN PROGRESS] add class 'active' to the clicked link */
-    console.log('clickedElement (with plus):' + clickedElement);
+
     clickedElement.classList.add('active');
+
+
     /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll(".posts .active");
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
+
+
+
     /* get 'href' attribute from the clicked link */
-
+    const articleSelector = ("href");
+    const href = clickedElement.getAttribute("href");
+    console.log(href);
+    
     /* find the correct article using the selector (value of 'href' attribute) */
-
+    const targetArticle = document.querySelector(href);
+    console.log(href);
     /* add class 'active' to the correct article */
 }
 
